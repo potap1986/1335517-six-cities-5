8eabb268b8e1a32f14d74e63269c3c61a614ad8a
potap1986 <potap1986@inbox.ru>
2020-10-01 22:39:12 +0300

    Личный проект: начинаем программировать

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
 const Settings = {
-  ERRORS_COUNT: 3
+  PLACES_COUNT: 30
 };
-
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, world!`;
 
 ReactDOM.render(
     <App
-      errorsCount={Settings.ERRORS_COUNT}
+      plasesCount={Settings.PLACES_COUNT}
     />,
     document.querySelector(`#root`)
 );
